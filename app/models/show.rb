816637b3ef5b5e4476bf6103842855be534c96a9
Nate Tan <nktan93@gmail.com>
2019-05-13 15:08:57 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,6 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    self.where("rating = ?", maximum(:rating))
+    self.where("rating = ?", maximum(:rating))[0]
   end
 end
