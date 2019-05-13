0990293b160a96f1c9ce41dea0dbe8f224bcc4b0
Nate Tan <nktan93@gmail.com>
2019-05-13 15:11:20 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -10,4 +10,8 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
     self.minimum(:rating)
   end
+  
+  def self.least_popular_show
+    self.where("rating = ?", minimum(:rating))[0]
+  end
 end
