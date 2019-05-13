778f09361bbb48d463602a16fa5056443ff23194
Nate Tan <nktan93@gmail.com>
2019-05-13 15:16:00 +0000

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -4,6 +4,26 @@ class Show < ActiveRecord::Base
   end
   
   def self.most_popular_show
-    self.where("rating = ?", maximum(:rating))
+    self.where("rating = ?", maximum(:rating))[0]
+  end
+  
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+  
+  def self.least_popular_show
+    self.where("rating = ?", minimum(:rating))[0]
+  end
+  
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+  
+  def self.popular_shows
+    self.where("rating > ?", 5)
+  end
+  
+  def self.shows_by_alphabetical_order
+    self.order(:name)
   end
 end
